14b5762881436eb64a2ec301943b3e1e03ea23c6
mjunyent <marc.juny@gmail.com>
2013-04-29 16:48:54 +0200

    Oscpack added to the project, separatethread working.

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,78 @@
 #include "Trinidad/Trinidad.h" //Trinidad contains scenes already done, just to plug & play to an sceneManager or wherever.
 #include "Vidian/VesselScene.h"
 
+#include "osc/OscReceivedElements.h"
+#include "osc/OscPacketListener.h"
+#include "ip/UdpSocket.h"
+
+#define PORT 7000
+
+class ExamplePacketListener : public osc::OscPacketListener {
+protected:
+
+    virtual void ProcessMessage( const osc::ReceivedMessage& m, 
+				const IpEndpointName& remoteEndpoint )
+    {
+        (void) remoteEndpoint; // suppress unused parameter warning
+
+        try{
+            // example of parsing single messages. osc::OsckPacketListener
+            // handles the bundle traversal.
+            
+            if( std::strcmp( m.AddressPattern(), "/test1" ) == 0 ){
+                // example #1 -- argument stream interface
+                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+                bool a1;
+                osc::int32 a2;
+                float a3;
+                const char *a4;
+                args >> a1 >> a2 >> a3 >> a4 >> osc::EndMessage;
+                
+                std::cout << "received '/test1' message with arguments: "
+                    << a1 << " " << a2 << " " << a3 << " " << a4 << "\n";
+                
+            }else if( std::strcmp( m.AddressPattern(), "/test2" ) == 0 ){
+                // example #2 -- argument iterator interface, supports
+                // reflection for overloaded messages (eg you can call 
+                // (*arg)->IsBool() to check if a bool was passed etc).
+                osc::ReceivedMessage::const_iterator arg = m.ArgumentsBegin();
+                bool a1 = (arg++)->AsBool();
+                int a2 = (arg++)->AsInt32();
+                float a3 = (arg++)->AsFloat();
+                const char *a4 = (arg++)->AsString();
+                if( arg != m.ArgumentsEnd() )
+                    throw osc::ExcessArgumentException();
+                
+                std::cout << "received '/test2' message with arguments: "
+                    << a1 << " " << a2 << " " << a3 << " " << a4 << "\n";
+            }
+        }catch( osc::Exception& e ){
+            // any parsing errors such as unexpected argument types, or 
+            // missing arguments get thrown as exceptions.
+            std::cout << "error while parsing message: "
+                << m.AddressPattern() << ": " << e.what() << "\n";
+        }
+    }
+};
+
+void RunListener(void *arg) {
+	
+    ExamplePacketListener listener;
+    UdpListeningReceiveSocket s(
+            IpEndpointName( IpEndpointName::ANY_ADDRESS, PORT ),
+            &listener );
+	s.RunUntilSigInt();
+
+}
+
 int main(void) {
 	int running = true;
 
 	OGL::init(1280, 720, 0, 0, 0, 32, 64, 1, "A2058", 3, 4, GLFW_WINDOW);
 //	glfwSwapInterval(20);
+
+	cout << "WTF" << endl;
+	glfwCreateThread(RunListener, NULL);
 
 	//Initialize some vars...
 	global::song = new SoundHandler("hearthShort5.mp3", 1024);
@@ -49,9 +116,9 @@ int main(void) {
 		global::manager->render();
 		glfwSwapBuffers();
 		randValue(100, 200);
-		if(glfwGetKey( 'Z' ))  global::song->setVel(global::song->playVel - 0.05);
+		if(glfwGetKey( 'Z' )) global::song->setVel(global::song->playVel - 0.05);
 		if(glfwGetKey( 'X' )) global::song->setVel(global::song->playVel + 0.05);
-		cout << global::song->playVel << endl;
+		//cout << global::song->playVel << endl;
 		running = !glfwGetKey( GLFW_KEY_ESC ) && glfwGetWindowParam( GLFW_OPENED );
 	}
 
